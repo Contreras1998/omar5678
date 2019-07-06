139156e17bcda1a4de6ca30a62556548b4578d80
contreras1998 <oandres1411@hotmail.com>
2019-07-06 12:12:43 +0100

    ghghghhghghg

---
+++ source/repos/ConsoleApp2/Program1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program1
+    {
+
+        static void Main(string[] args)
+        {
+
+            var person = new Person();
+            person.BirthDate = new DateTime(2000, 1, 11);
+            Console.WriteLine(person.Age);
+
+
+
+
+        }
+
+    }
+
+
+}
---
+++ source/repos/SaturdayProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SaturdayProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
